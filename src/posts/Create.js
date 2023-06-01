6a3b750193b980dcb1612be0a36a5f43acfb2e0b
otavio_dm <102213097+magnaniotavio@users.noreply.github.com>
2023-06-01 12:54:11 -0300

    modified:   src/posts/Create.js

--- src/posts/Create.js
+++ src/posts/Create.js
@@ -72,7 +72,7 @@ export default function CreatePost() {
         }; 
         console.log(newPosts)
 
-    axios.post('https://e-commerce-model.onrender.com/posts/add', newPosts)
+    axios.post('https://e-commerce-model.onrender.com/add', newPosts)
       .then(res => {
         console.log(res.data);
         navigate(`/${newPosts.newTitle}`);
