05b7799414b09f8866e67a1c9be8014fa4352521
otavio_dm <102213097+magnaniotavio@users.noreply.github.com>
2023-06-27 19:37:23 -0300

    Added many comments.

--- src/searchComponents/FiltersNavBar.js
+++ src/searchComponents/FiltersNavBar.js
@@ -1,39 +1,49 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+/* In this component we return the functions which are necessary for our search, and which will
+   be called in our App.js component */
+
 export const NavbarFilterFunctions = () => {
+
     const [selectedFilters, setSelectedFilters] = useState([]);
     const navigate = useNavigate();
-    
-const handleFilterChange = (filter) => {
-        const updatedFilters = [...selectedFilters];
-        const filterIndex = updatedFilters.indexOf(filter);
-        if (filterIndex === -1) {
-          updatedFilters.push(filter);
-        } else {
-          updatedFilters.splice(filterIndex, 1);
-        }
-        setSelectedFilters(updatedFilters);
-      };
 
- const handleArrayFilterChange = (filters) => {
-        setSelectedFilters(prevFilters => {
-          const updatedFilters = [...prevFilters];
-      
-          filters.forEach(filter => {
-            const filterIndex = updatedFilters.indexOf(filter);
-            if (filterIndex === -1) {
-              updatedFilters.push(filter);
-            } else {
-              updatedFilters.splice(filterIndex, 1);
-            }
-          });
-      
-          return updatedFilters;
-        });
-      };
-      
-      const HandleSearch = (e) => {
+// Changes the filters. This will be called as a prop by the NavBarMenu component
+
+      /* Handles one filter only;
+         Example: handleFilterChange('targetPublic:Feminine')} */
+      const handleFilterChange = (filter) => {
+              const updatedFilters = [...selectedFilters];
+              const filterIndex = updatedFilters.indexOf(filter);
+              if (filterIndex === -1) {
+                updatedFilters.push(filter);
+              } else {
+                updatedFilters.splice(filterIndex, 1);
+              }
+              setSelectedFilters(updatedFilters);
+            };
+
+      /* Handles filter changes when we select multiple filters
+         Example: handleArrayFilterChange(['classification:TrouserM', 'classification:TrouserF']) */
+       const handleArrayFilterChange = (filters) => {
+              setSelectedFilters(prevFilters => {
+                // Creates array with previous filters
+                const updatedFilters = [...prevFilters];
+                filters.forEach(filter => {
+                  const filterIndex = updatedFilters.indexOf(filter);
+                  // Pushes new filters
+                  if (filterIndex === -1) {
+                    updatedFilters.push(filter);
+                  } else {
+                    updatedFilters.splice(filterIndex, 1);
+                  }
+                });
+                return updatedFilters;
+              });
+            };
+            
+  const HandleSearch = (e) => {
         e.preventDefault();
         const query = selectedFilters.join(',');
         navigate(`/products/${query}`);
